ea819d2567d5d441b77dbb41265a319d97cdcc66
Tejas khope <tejaskhope600@gmail.com>
2025-06-19 13:03:11 +0530

    Start

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ['b2b', 'b2c'], default: 'b2c' }
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+export default mongoose.model('User', userSchema);
